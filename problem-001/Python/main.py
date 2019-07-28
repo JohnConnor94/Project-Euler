5b71ba4d137cfc1b547221caa87c18e1334f5450
JohnConnor94 <ignazio.messina.994@gmail.com>
2019-07-28 13:29:19 +0200

    First problem solved in Python. More time spent on using and learning pycharm ide and tools than solving this problem.

---
+++ problem-001/Python/main.py
@@ -0,0 +1,30 @@
+def sol1(limit) -> int:
+    """
+    Simple solution with for, C-stylish
+    """
+    total = 0
+    for x in range(limit):
+        if x % 3 == 0 or x % 5 == 0:
+            total += x
+    return total
+
+
+def sol2(limit) -> int:
+    """
+    A little more pythonic solution with list comprehension to generate the list
+    """
+    total = 0
+    numbers = [x for x in range(limit) if x % 3 == 0 or x % 5 == 0]
+    for x in numbers:
+        total += x
+    return total
+
+
+def main():
+    limit = 1000
+    print(sol1(limit))
+    print(sol2(limit))
+    pass
+
+
+main()
